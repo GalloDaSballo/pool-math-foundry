fbe9605c0143f5045253e3bd68845b23e0581a38
Entreprenerd <alex@entreprenerd.xyz>
2023-05-16 08:46:56 +0200

    chore: initial

--- test/Counter.t.sol
+++ test/Counter.t.sol
@@ -2,23 +2,32 @@
 pragma solidity ^0.8.13;
 
 import "forge-std/Test.sol";
-import "../src/Counter.sol";
+import "forge-std/console2.sol";
+
+interface IFactory {
+    function createPair(address tokenA, address tokenB, bool stable) external returns (address pair);
+}
+
+interface IPool {
+    function mint(address to) external returns (uint liquidity);
+    function getAmountOut(uint amountIn, address tokenIn) external view returns (uint);
+}
 
 contract CounterTest is Test {
-    Counter public counter;
+    uint256 MAX_BPS = 10_000;
+    uint256 STABLE_FEES = 5;
+    uint256 VARIABLE_FEES = 5;
+
+    IFactory factory = IFactory(0x25CbdDb98b35ab1FF77413456B31EC81A6B6B746);
 
     function setUp() public {
-        counter = new Counter();
-        counter.setNumber(0);
+        
+
+        address newPool = factory.createPair(tokenA, tokenB, stable);
     }
 
-    function testIncrement() public {
-        counter.increment();
-        assertEq(counter.number(), 1);
-    }
-
-    function testSetNumber(uint256 x) public {
-        counter.setNumber(x);
-        assertEq(counter.number(), x);
+    function _deposit(uint256 amountIn, uint256 amountOut) internal {
+        // Deposit those on Pair
+        // Then call mint
     }
 }
